b22692312c4f9b6c85f033d9de8933969a1ec317
MonodeepMono <monodeep301@gmail.com>
2024-03-06 13:12:47 +0530

    MIS

--- MIS_TRACKER.py
+++ MIS_TRACKER.py
@@ -19,36 +19,44 @@ mycursor = mydb.cursor()
 conn = mycursor.execute
 
 sql_query = """
-                  SELECT 
+           SELECT 
     DATE_FORMAT(c.consult_datetime, '%d-%m-%y') as Date,
-    ap.full_name as DoctorName,
+    ap.full_name as PMS_Name,
     l.name as ClinicName,
-    SUM(CASE WHEN c.title LIKE '%First Consultation%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as NewConsults,
-    SUM(CASE WHEN c.title LIKE '%Follow up%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as FollowupConsults, 
-    SUM(CASE WHEN c.title LIKE '%CRP%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as CRPConsults, 
-    SUM(CASE WHEN c.title LIKE '%PRC%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as PRCConsults,
+    SUM(CASE WHEN c.title LIKE '%First Consultation%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as PMS_New_Consults,
+    SUM(CASE WHEN c.title LIKE '%Follow up%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as PMS_Follow_up_Consults,
+    SUM(CASE WHEN c.title LIKE '%Physio%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as Physio_Consults,
+    SUM(CASE WHEN c.title LIKE '%Nutrition%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as Nutrition_Consults,
+    SUM(CASE WHEN c.title LIKE '%Psychology%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as Psychology_Consults,
+    SUM(CASE WHEN c.title LIKE '%CRP%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as CRP_Consults, 
+    SUM(CASE WHEN c.title LIKE '%PRC%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as PRC_Consults,
+    SUM(CASE WHEN c.title LIKE '%CRPPrescribed%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as CRPPrescribed,
+    SUM(CASE WHEN c.title LIKE '%SingleSpec%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as SingleSpec, 
+    SUM(CASE WHEN c.title LIKE '%CRPBooked%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as CRPBooked,
+    SUM(CASE WHEN c.title LIKE '%MultiSpecCRPBooked%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as MultiSpecCRPBooked,
+    SUM(CASE WHEN c.title LIKE '%PhysioCRP%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as PhysioCRP, 
+    SUM(CASE WHEN c.title LIKE '%PRCDone%' AND c.patient_attendance_status = 'show' THEN 1 ELSE 0 END) as PRCDone,
     (SELECT SUM(amount) 
      FROM nivaancare_production.consultation 
      WHERE (title LIKE '%First Consultation%' OR title LIKE '%Follow up%')
      AND patient_attendance_status = 'show'
      AND consultant_id = c.consultant_id
-     AND location_id = c.location_id
-     AND DATE_FORMAT(consult_datetime, '%d:%m:%y') = DATE_FORMAT(c.consult_datetime, '%d:%m:%y')
-    ) as Revenue,
+     AND DATE_FORMAT(consult_datetime, '%d-%m-%y') = DATE_FORMAT(c.consult_datetime, '%d-%m-%y')
+    ) as Consult_GMV,
     (SELECT SUM(amount) 
      FROM nivaancare_production.consultation 
-     WHERE title LIKE '%CRP%' AND patient_attendance_status = 'show'
+     WHERE title LIKE '%CRP%' 
+     AND patient_attendance_status = 'show'
      AND consultant_id = c.consultant_id
-     AND location_id = c.location_id
-     AND DATE_FORMAT(consult_datetime, '%d:%m:%y') = DATE_FORMAT(c.consult_datetime, '%d:%m:%y')
-    ) as CRP_Revenue,
+     AND DATE_FORMAT(consult_datetime, '%d-%m-%y') = DATE_FORMAT(c.consult_datetime, '%d-%m-%y')
+    ) as CRP_GMV,
     (SELECT SUM(amount) 
      FROM nivaancare_production.consultation 
-     WHERE title LIKE '%PRC%' AND patient_attendance_status = 'show'
+     WHERE title LIKE '%PRC%' 
+     AND patient_attendance_status = 'show'
      AND consultant_id = c.consultant_id
-     AND location_id = c.location_id
-     AND DATE_FORMAT(consult_datetime, '%d:%m:%y') = DATE_FORMAT(c.consult_datetime, '%d:%m:%y')
-    ) as PRC_Revenue
+     AND DATE_FORMAT(consult_datetime, '%d-%m-%y') = DATE_FORMAT(c.consult_datetime, '%d-%m-%y')
+    ) as PRC_GMV
 FROM 
     nivaancare_production.consultation c
 LEFT JOIN 
@@ -60,21 +68,21 @@ LEFT JOIN
 ON 
     c.location_id = l.id
 GROUP BY 
-    Date, DoctorName, City
+    Date, PMS_Name, ClinicName
 ORDER BY 
-    STR_TO_DATE(Date, '%d:%m:%y') DESC;
+    STR_TO_DATE(Date, '%d-%m-%y') DESC;
 
 """
 df_DATA = pd.read_sql_query(sql_query,mydb)
 df_DATA
-df_Final = df_DATA [['Date','DoctorName','ClinicName','NewConsults','FollowupConsults','Revenue', 'CRPConsults','CRP_Revenue',   'PRCConsults','PRC_Revenue']]
+df_Final = df_DATA [["Date", "PMS_Name", "ClinicName", "PMS_New_Consults", "PMS_Follow_up_Consults", "Consult_GMV", "CRP_Consults", "CRP_GMV", "PRC_Consults", "PRC_GMV", "Physio_Consults", "Nutrition_Consults", "Psychology_Consults"]]
 
 df_Final['Date'] = pd.to_datetime(df_Final['Date'], format='%d-%m-%y').dt.strftime('%Y-%m-%d')
 
-df_Final_filtered = df_Final.loc[(df_Final['NewConsults'] != 0) |
-                                 (df_Final['FollowupConsults'] != 0) |
-                                 (df_Final['CRPConsults'] != 0) |
-                                 (df_Final['PRCConsults'] != 0)]
+df_Final_filtered = df_Final.loc[(df_Final['PMS_New_Consults'] != 0) |
+                                 (df_Final['PMS_Follow_up_Consults'] != 0) |
+                                 (df_Final['CRP_Consults'] != 0) |
+                                 (df_Final['PRC_Consults'] != 0)]
 df_Final_filtered
 df_Final_filtered_sorted = df_Final_filtered.sort_values(by='Date', ascending=False)
 df_Final_filtered_sorted
@@ -101,11 +109,11 @@ spreadsheetId = '1ldw_7GtmHxRy_tYTaajyeItqezcLRwWztqxnWHYP528'
 sh = gc.open_by_key(spreadsheetId)
 
 # Define the sheet name and CSV file path
-sheetName = 'RAW'
+sheetName = 'Running_MIS_Tracker'
 csvFile = 'MIS_Tracker.csv'
 
 # Clear existing values in the specified range
-sh.values_clear("'RAW'!A:J")
+sh.values_clear("'Running_MIS_Tracker'!A:M")
 
 # Read the CSV file with UTF-8 encoding and update the spreadsheet with its values
 with open(csvFile, 'r', encoding='utf-8') as file:
